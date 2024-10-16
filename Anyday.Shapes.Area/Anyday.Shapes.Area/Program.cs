73659b8906d8783763790747dbcdca120e7ab9cb
Simran <sp@anyday.io>
2024-10-16 09:06:28 +0700

    Add code

---
+++ Anyday.Shapes.Area/Anyday.Shapes.Area/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Anyday.Shapes.Area;
+
+var circle = new DerivedShapeCircle
+{
+    Radius = 10 //Get rid of this.
+};
+//I would like to initialize the radius from constructor.
+var circleArea = circle.CalculateArea(circle.Radius);
+
+Console.WriteLine("Area of circle is :" + circleArea.ToString());
+
+var rectangle = new DerivedShapeRectangle
+{
+    Width = 10, //Get rid of these
+    Height = 10
+}; //I would like to initialize the radius from constructor.
+var rectangleArea = rectangle.CalculateArea(rectangle.Width, rectangle.Width);
+
+Console.WriteLine("Area of rectangle is:" + rectangleArea);
+
+//Add more shape Rectangle and calculate it's Area.
+
+//Add more shape Triangle and calculate it's Area.
+
+
---
+++ Anyday.Shapes.Area/Anyday.Shapes.Area/ShapeModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Anyday.Shapes.Area
+{
+
+    //Should we make it abstract?
+    public class BaseShapes
+    {
+        //
+    }
+
+    public class DerivedShapeCircle : BaseShapes
+    {
+        public int Radius;
+
+        public int CalculateArea(int radius)
+        {
+            //Calculate and return area;
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DerivedShapeRectangle : BaseShapes
+    {
+        public int Width;
+        public int Height;
+
+        public int CalculateArea(int width, int height)
+        {
+            //Calculate and return area;
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
